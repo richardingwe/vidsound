7f465579f1a075b6b8aef515dfa325e642d2d2c6
Richard Ingwe <richardingwe5@gmail.com>
2022-11-20 10:17:28 +0100

    workspaces

--- js/script.js
+++ js/script.js
@@ -1,11 +1,6 @@
 const profileDropdown = document.querySelector('.profile_dropdown');
 const dropdownMenu = document.querySelector('._dropdown-menu');
 
-window.addEventListener('click', (e) => {
-    if (e.target !== profileDropdown) {
-        dropdownMenu.classList.toggle('show');
-    }
-});
 
 profileDropdown.addEventListener('click', () => {
     dropdownMenu.classList.toggle('show');
